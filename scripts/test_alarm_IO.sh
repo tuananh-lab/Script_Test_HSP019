b7e99062fac3439a0233fefcad5f7e1bdb23d65f
anhtt <anhtt@hsptek.com>
2024-09-27 16:17:14 +0700

    Fix alarm

--- scripts/test_alarm_IO.sh
+++ scripts/test_alarm_IO.sh
@@ -3,32 +3,42 @@
 # Set current directory
 current_dir="$(cd "$(dirname "$0")" && pwd)"
 parent_dir="$(dirname "$current_dir")"
-# result_dir="$parent_dir/result"
-
-# # Create the "result" directory if it doesn't exist
-# mkdir -p "$result_dir"
-
-# # Log file location
-# log_file="$result_dir/test_alarm_IO_results.txt"
 
 # Initialize result variable
 result=0
 
 # Define GPIO pins
-gpio_pins=("gpio457" "gpio459")
+gpio_pins=("457" "459")
 
 # Define logging function
 log() {
     echo "$1"
 }
 
-# Define function to test GPIO value
-test_gpio() {
+# Function to export GPIO if not already exported
+export_gpio() {
+    local gpio_pin="$1"
+    if [ ! -d "/sys/class/gpio/gpio$gpio_pin" ]; then
+        log "Exporting GPIO $gpio_pin"
+        echo "$gpio_pin" > "/sys/class/gpio/export"
+        sleep 1
+    fi
+}
+
+# Function to set GPIO direction
+set_gpio_direction() {
+    local gpio_pin="$1"
+    local direction="$2"
+    log "Setting GPIO $gpio_pin direction to $direction"
+    echo "$direction" > "/sys/class/gpio/gpio$gpio_pin/direction"
+}
+
+# Function to set GPIO value
+set_gpio_value() {
     local gpio_pin="$1"
     local value="$2"
-
     log "Setting $gpio_pin to $value"
-    echo "$value" > "/sys/class/gpio/$gpio_pin/value"
+    echo "$value" > "/sys/class/gpio/gpio$gpio_pin/value"
 }
 
 # Colors
@@ -40,38 +50,23 @@ NC='\033[0m' # No Color
 # Start testing
 log "Testing Alarm IO"
 
-# Check GPIO pins
+# Check GPIO pins and prepare them
 for gpio_pin in "${gpio_pins[@]}"; do
-    if [ ! -e "/sys/class/gpio/$gpio_pin/value" ]; then
-        log "GPIO pin $gpio_pin does not exist."
-        result=1
-    fi
+    export_gpio "$gpio_pin"
+    set_gpio_direction "$gpio_pin" "out"
 done
 
 # Test GPIO values
-if [ $result -eq 0 ]; then
-    # Set and reset GPIO values
-    for gpio_pin in "${gpio_pins[@]}"; do
-        test_gpio "$gpio_pin" 1
-        sleep 1
-        test_gpio "$gpio_pin" 0
-        sleep 1
-    done
+for gpio_pin in "${gpio_pins[@]}"; do
+    set_gpio_value "$gpio_pin" 1
+    sleep 1
+    set_gpio_value "$gpio_pin" 0
+    sleep 1
+done
 
-    log "Alarm IO test completed. Listen for relay clicks."
+log "Alarm IO test completed. Listen for relay clicks."
 
-    # Set result to PASS
-    test_result="PASS"
-else
-    # Set result to FAIL
-    test_result="FAIL"
-fi
-
-# Print the result with color and bold
-if [ "$test_result" == "FAIL" ]; then
-    echo -e "Test result: ${RED}${BOLD}FAIL${NC}"
-else
-    echo -e "Test result: ${GREEN}${BOLD}PASS${NC}"
-fi
+# Set result to PASS and print result with color and bold
+echo -e "Test result: ${GREEN}${BOLD}PASS${NC}"
 
 exit $result
